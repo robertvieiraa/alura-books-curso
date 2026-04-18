1d2b5cb64161d85d2fc99afe04c69cfaff6ae942
Robert <112430289+robertvieiraa@users.noreply.github.com>
2026-04-18 08:40:21 -0300

    Filtrei os livros na tela

--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -7,5 +7,5 @@ function filtrarLivros() {
     const categoria = elementoBtn.value;
 
     let livrosFiltrados = livros.filter(livro => livro.categoria == categoria)
-    console.table(livrosFiltrados)
+    exibirOsLivrosNaTela(livrosFiltrados)
 }
