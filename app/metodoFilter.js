b6c29a928691eef33c616e5c83df8bc3b43527b4
Robert <112430289+robertvieiraa@users.noreply.github.com>
2026-04-27 21:36:22 -0300

    Manipulei a exibição do valor total de livros e organizei o código

--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -6,6 +6,26 @@ function filtrarLivros() {
     const elementoBtn = document.getElementById(this.id)
     const categoria = elementoBtn.value;
 
-    let livrosFiltrados = categoria == 'disponivel' ? livros.filter(livro => livro.quantidade > 0) : livros.filter(livro => livro.categoria == categoria)
+    let livrosFiltrados = categoria == 'disponivel' ? filtrarPorDisponibilidade() : filtrarPorCategoria(categoria)
     exibirOsLivrosNaTela(livrosFiltrados)
+
+    if (categoria == 'disponivel') {
+        exibirValorTotalDosLivrosDisponiveisNaTela()
+    }
+}
+
+function filtrarPorCategoria(categoria) {
+    return livros.filter(livro => livro.categoria == categoria);
+}
+
+function filtrarPorDisponibilidade() {
+    return livros.filter(livro => livro.quantidade > 0);
+}
+
+function exibirValorTotalDosLivrosDisponiveisNaTela() {
+    elementoComValorTotalDeLivrosDisponiveis.innerHTML = `
+    <div class="livros__disponiveis">
+      <p>Todos os livros disponíveis por R$ <span id="valor">299,00</span></p>
+    </div>
+    `
 }
